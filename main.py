3a8f36684c4c7dcfb8afdd668fb0cf450f8b60a2
Bebel-O-Rama <bebel-o-rama@hotmail.com>
2021-09-25 16:22:11 -0400

    Added a method to output graph in pdf

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import pandas as panda
 import matplotlib.pyplot as plot
 from dataclasses import dataclass
 
+SEPAL_LENGTH = "sepal_length"
+SEPAL_WIDTH = "sepal_width"
+PETAL_LENGTH = "petal_length"
+PETAL_WIDTH = "petal_width"
+
 
 @dataclass
 class IRISClass:
@@ -9,34 +14,41 @@ class IRISClass:
     sepal_width: float
     petal_length: float
     petal_width: float
+    class_data: float
     species: str
 
 
-def getCentreClasse(dataset, nomClasse):
+def getDataClasse(dataset, nomClasse):
     classe = dataset[dataset["species"] == nomClasse]
-    classe = classe[["sepal_length", "sepal_width", "petal_length", "petal_width"]]
-    sepalLengthMean = round(classe["sepal_length"].mean(), 1)
-    sepalWidthMean = round(classe["sepal_width"].mean(), 1)
-    petalLengthMean = round(classe["petal_length"].mean(), 1)
-    petalWidthMean = round(classe["petal_width"].mean(), 1)
-    return IRISClass(sepalLengthMean, sepalWidthMean, petalLengthMean, petalWidthMean, nomClasse)
+    classe = classe[[SEPAL_LENGTH, SEPAL_WIDTH, PETAL_LENGTH, PETAL_WIDTH]]
+    sepalLengthMean = round(classe[SEPAL_LENGTH].mean(), 1)
+    sepalWidthMean = round(classe[SEPAL_WIDTH].mean(), 1)
+    petalLengthMean = round(classe[PETAL_LENGTH].mean(), 1)
+    petalWidthMean = round(classe[PETAL_WIDTH].mean(), 1)
+    return IRISClass(sepalLengthMean, sepalWidthMean, petalLengthMean, petalWidthMean, classe, nomClasse)
 
 
 # Methode 1a
 
 # TODO: Rajouter des attributs avec la distance intra et interclasse
 def methodeUnA(iris):
-    setosaCentre = getCentreClasse(iris, "setosa")
-    versicolorCentre = getCentreClasse(iris, "versicolor")
-    ### air_quality["intra_class_distance"] = (air_quality["station_paris"] / air_quality["station_antwerp"])
+    setosa_data_centre = getDataClasse(iris, "setosa")
+    versicolor_data_centre = getDataClasse(iris, "versicolor")
+    virginica_data_centre = getDataClasse(iris, "virginica")
 
-    versicolor = iris[iris["species"] == "versicolor"]
-    versicolor = versicolor[["sepal_length", "sepal_width", "petal_length", "petal_width"]]
-    versicolor.plot.scatter(x="sepal_length",y="petal_length",alpha=0.5)
-    print(versicolor)
-    plot.savefig("output/test.pdf")
-    # print(iris.to_string())
+    setosa_data_centre = saveGraph(SEPAL_LENGTH, PETAL_LENGTH, "test1", "test_setosa", setosa_data_centre.class_data)
+    versicolor_plot = saveGraph(SEPAL_LENGTH, PETAL_LENGTH, "test2", "test_versicolor", versicolor_data_centre.class_data)
+    virginica_data_centre = saveGraph(SEPAL_LENGTH, PETAL_LENGTH, "test3", "test_virginica", virginica_data_centre.class_data)
+
     return
+
+
+def saveGraph(x_axis, y_axis, plot_name, file_name, data):
+    data.plot.scatter(x=x_axis, y=y_axis, alpha=0.5)
+    plot.title(plot_name)
+    plot.savefig("output/"+ file_name + ".pdf")
+    return
+
 
 def main():
     iris = panda.read_csv("data/iris.csv")
